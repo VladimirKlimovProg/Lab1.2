9a15c80be6b1bdb189b4d7604787efedba9f8918
Vladimir Klimov <vladimirklimoff@yandex.ru>
2018-10-09 21:38:38 +0500

    init

--- Lab 1.2/Program.cs
+++ Lab 1.2/Program.cs
@@ -10,6 +10,33 @@ namespace Lab_1._2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите значение x:");
+            if (!double.TryParse(Console.ReadLine(), out double x))
+            {
+                Console.WriteLine("Некорректный ввод. Необходимо вещественное число. В качестве разделителя используйте запятую.");
+            }
+            else
+            {
+                Console.WriteLine("Введите значение y:");
+                if (!double.TryParse(Console.ReadLine(), out double y))
+                {
+                    Console.WriteLine("Некорректный ввод. Необходимо вещественное число. В качестве разделителя используйте запятую.");
+                }
+                else
+                {
+                    bool result = (x * x + y * y <= 1) && x >= 0;
+                    if (result)
+                    {
+                        Console.WriteLine($"Точка ({x}, {y}) принадлежит требуемой области");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Точка ({x}, {y}) находится за пределами требуемой области");
+                    }
+                }
+                
+            }
+            Console.ReadLine();
         }
     }
 }
